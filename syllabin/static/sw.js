8837958a60b48c528f68076a0ba4f87886c353b1
IlyaMZP <ilya.kovchenkov@gmail.com>
2021-03-18 09:38:09 +0300

    Return to workbox...

--- syllabin/static/sw.js
+++ syllabin/static/sw.js
@@ -1,9 +1,12 @@
-'use strict';
-importScripts('/static/js/sw-toolbox.js');
+//This is the service worker with the Advanced caching
 
-toolbox.precache(["/","/static/open-iconic/font/fonts/open-iconic.woff","/static/css/bootstrap.min.css", "/static/js/app.js", "/static/js/bootstrap.bundle.min.js", "/static/js/jquery-3.5.1.min.js", "/static/open-iconic/font/css/open-iconic-bootstrap.css"]);
-toolbox.router.get('/static/*', toolbox.cacheFirst);
-toolbox.router.get('/*', toolbox.networkFirst, { networkTimeoutSeconds: 4});
+importScripts('https://storage.googleapis.com/workbox-cdn/releases/6.1.1/workbox-sw.js');
+
+const HTML_CACHE = "html";
+const JS_CACHE = "javascript";
+const STYLE_CACHE = "stylesheets";
+const IMAGE_CACHE = "images";
+const FONT_CACHE = "fonts";
 
 self.addEventListener('push', event => {
     console.log('[Service Worker] Push Received.');
@@ -16,6 +19,68 @@ self.addEventListener('push', event => {
     };
     event.waitUntil(self.registration.showNotification(title, options))
 });
-self.addEventListener('install', function(event) {
-  self.skipWaiting();
-});
+
+workbox.routing.registerRoute(
+  ({event}) => event.request.destination === 'document',
+  new workbox.strategies.NetworkFirst({
+    cacheName: HTML_CACHE,
+    networkTimeoutSeconds: 3,
+    plugins: [
+      new workbox.expiration.ExpirationPlugin({
+        maxEntries: 10,
+      }),
+    ],
+  })
+);
+
+workbox.routing.registerRoute(
+  ({event}) => event.request.destination === 'script',
+  new workbox.strategies.StaleWhileRevalidate({
+    cacheName: JS_CACHE,
+    plugins: [
+      new workbox.expiration.ExpirationPlugin({
+        maxEntries: 15,
+        maxAgeSeconds: 7 * 24 * 60 * 60,
+      }),
+    ],
+  })
+);
+
+workbox.routing.registerRoute(
+  ({event}) => event.request.destination === 'style',
+  new workbox.strategies.StaleWhileRevalidate({
+    cacheName: STYLE_CACHE,
+    plugins: [
+      new workbox.expiration.ExpirationPlugin({
+        maxEntries: 15,
+      }),
+    ],
+  })
+);
+
+workbox.routing.registerRoute(
+  ({event}) => event.request.destination === 'image',
+  new workbox.strategies.StaleWhileRevalidate({
+    cacheName: IMAGE_CACHE,
+    plugins: [
+      new workbox.expiration.ExpirationPlugin({
+        maxEntries: 15,
+      }),
+    ],
+  })
+);
+
+workbox.routing.registerRoute(
+  ({event}) => event.request.destination === 'font',
+  new workbox.strategies.StaleWhileRevalidate({
+    cacheName: FONT_CACHE,
+    plugins: [
+      new workbox.expiration.ExpirationPlugin({
+        maxEntries: 15,
+      }),
+    ],
+  })
+);
+
+workbox.core.skipWaiting();
+workbox.core.clientsClaim();
